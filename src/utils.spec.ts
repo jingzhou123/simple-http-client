08b34f61bb6d4d3d1380760f32632856ca20d877
既望 <jingzhou.jz@alibaba-inc.com>
2019-01-17 19:24:01 +0800

    spec for intercept response

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,8 +1,28 @@
-import { IHTTPRequestInterceptor } from './interface';
+import { IHTTPRequestInterceptor, IHTTPResponseInterceptor } from './interface';
 import { HTTPRequest } from './HTTPRequest';
-import { interceptRequest } from './utils';
+import { interceptRequest, interceptResponse } from './utils';
+import { HTTPResponse } from './HTTPResponse';
 
 describe('utils', () => {
+  it('interceptResponse', () => {
+    class Interceptor1 implements IHTTPResponseInterceptor{
+      intercept(response: any): any {
+        response.response = response.response + 'data1'
+        return Promise.resolve(response);
+      }
+    }
+    class Interceptor2 implements IHTTPResponseInterceptor{
+      intercept(response: any): any {
+        response.response = response.response + 'data2'
+        return response;
+      }
+    }
+    const interceptors: IHTTPResponseInterceptor[] = [
+      new Interceptor1(), new Interceptor2()
+    ]
+    const result = interceptResponse(new HTTPResponse('any'), interceptors)
+    expect(result).resolves.toEqual({ response: 'anydata1data2' })
+  })
   it('interceptRequest', () => {
     class Interceptor1 implements IHTTPRequestInterceptor  {
       intercept(request: HTTPRequest): HTTPRequest | Promise<HTTPRequest> {
